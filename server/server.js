f1534ec196f433a41268669ee787556b9e2971ff
JEN-chad <jenishj436@gmail.com>
2026-06-23 19:17:43 +0530

    updated cors

--- server/server.js
+++ server/server.js
@@ -18,7 +18,14 @@ connectDB();
 const app = express();
 
 // Body parsers and CORS
-app.use(cors());
+app.use(
+  cors({
+    origin: process.env.CLIENT_URL,
+    credentials: true,
+  })
+);
+
+
 app.use(express.json());
 
 // API Routes
@@ -46,8 +53,9 @@ const PORT = process.env.PORT || 5000;
 const server = createServer(app);
 const io = new Server(server, {
   cors: {
-    origin: '*',
-    methods: ['GET', 'POST'],
+    origin: process.env.CLIENT_URL,
+    credentials: true,
+    methods: ["GET", "POST"],
   },
 });
 
